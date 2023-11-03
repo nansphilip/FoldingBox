df895f9d3fe4e5656f32109113ad3a2bdbf1e15a
nansphilip <nansphilip@gmail.com>
2023-11-03 12:16:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const sections = document.querySelectorAll("section");
+const arrows = document.querySelectorAll("img");
+
+// For each section, add an event listener to the click event
+for (let i = 0; i < sections.length; i++) {
+    sections[i].addEventListener("click", () => {
+        
+        // If the section contains '.active', remove it and set the height to default (32px)
+        if (sections[i].classList.contains("active")) {
+            sections[i].classList.remove("active");
+            sections[i].style.height = `32px`;
+            arrows[i].style.transform = 'rotate(0deg)';
+
+        }
+        
+        // Else, remove '.active' and set the height to default (32px) on every sections,
+        // then add '.active' and set the measured height to the clicked section
+        else {
+            for (let e = 0; e < sections.length; e++) {
+                sections[e].classList.remove("active");
+                sections[e].style.height = `32px`;
+                arrows[e].style.transform = 'rotate(0deg)';
+            }
+
+            // const hrHeight = sections[i].querySelector('hr').offsetHeight;
+            // const pHeight = sections[i].querySelector('p').offsetHeight;
+            // const totalHeight = hrHeight + pHeight + 32;
+            let totalHeight = 0;
+            Array.from(sections[i].children).forEach((child) => {
+                totalHeight += child.offsetHeight;
+            })
+            console.log(Array.from(sections[i].children));
+            console.log(totalHeight);
+
+            sections[i].style.height = `${totalHeight}px`;
+            arrows[i].style.transform = 'rotate(90deg)';
+            sections[i].classList.add("active");
+        }
+    });
+}
